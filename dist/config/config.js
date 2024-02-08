c14233f38459ca1c6349d591c905fa52fa28559e
Joe Ma <joema.dev@gmail.com>
2024-02-07 16:57:36 -0800

    delete build folders

--- dist/config/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.reviewers = void 0;
-exports.reviewers = [
-    'reviewer_1',
-    'reviewer_2',
-    'reviewer_3',
-];
-//# sourceMappingURL=config.js.map
